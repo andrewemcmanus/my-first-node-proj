2421508bc96f135fc8d5c996687d7ded99c0b5e8
andrewemcmanus <60327690+andrewemcmanus@users.noreply.github.com>
2020-11-12 15:01:31 -0500

    new module

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// const myModule = require('./myModule.js');
+const {add, subtract, addAgain} = require('./myModule.js');
 let name = 'Andrew';
-console.log(name);
 
 function printName(person) {
     return `Hello, ${person}`;
 };
-console.log(printName(name));
+
+// console.log(myModule.beBasic());
+console.log(add(5, 50));
+console.log(subtract(10, 20));
+console.log(addAgain(27, 5913));
--- myModule.js
+++ myModule.js
@@ -1,1 +1,21 @@
-module.exports.beBasic = () => "That's so fetch!"
+// module.exports.beBasic = () => "That's so fetch!"
+const beBasic = () => "That's so fetch!";
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num2 - num1;
+}
+
+function addAgain(num1, num2) {
+  return num1 + num2;
+}
+
+module.exports = {
+  beBasic,
+  add,
+  subtract,
+  addAgain
+};
